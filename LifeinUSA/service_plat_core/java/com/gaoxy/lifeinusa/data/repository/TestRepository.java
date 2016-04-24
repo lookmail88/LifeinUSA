bb513fa4315faf3740748a6e2429abada8591bbd
lookmail88@gmail.com <lookmail88@gmail.com>
2016-04-24 16:15:04 -0700

    update pom.xml

---
+++ LifeinUSA/service_plat_core/java/com/gaoxy/lifeinusa/data/repository/TestRepository.java
@@ -0,0 +1,22 @@
+/**
+* <p>Title: TestRepository.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2015</p>
+* <p>Company: gaoxy</p>
+* @author steven.gaoxy
+* @date Apr 23, 2016
+* @version 1.0
+*/
+
+package com.gaoxy.lifeinusa.data.repository;
+
+/**
+* <p>Title: TestRepository</p>
+* <p>Description: </p>
+* <p>Company: gaoxy</p>
+* @author steven.gaoxy
+* @date Apr 23, 2016
+*/
+public interface TestRepository {
+
+}
---
+++ LifeinUSA/service_plat_core/java/com/gaoxy/lifeinusa/data/service/TestDataService.java
@@ -0,0 +1,22 @@
+/**
+* <p>Title: TestDataService.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2015</p>
+* <p>Company: gaoxy</p>
+* @author steven.gaoxy
+* @date Apr 23, 2016
+* @version 1.0
+*/
+
+package com.gaoxy.lifeinusa.data.service;
+
+/**
+* <p>Title: TestDataService</p>
+* <p>Description: </p>
+* <p>Company: gaoxy</p>
+* @author steven.gaoxy
+* @date Apr 23, 2016
+*/
+public class TestDataService {
+
+}
